f3b820adc78d2e8115c51317be4d1c7f76bd87f1
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2017-11-02 01:05:28 +0100

    Finished moves (except Pawn promotion)

--- sk2_project/src/figures/Pawn.java
+++ sk2_project/src/figures/Pawn.java
@@ -10,17 +10,47 @@ package figures;
  * @author mateusz
  */
 public class Pawn extends Figure {
-
     public Pawn(String colour){
         super("pawn",colour);
     }
     
     @Override
     public boolean isMoveAllowed(int newX, int newY){
+        if(newX != xPosition){
+            return false;
+        }
+        else if((yPosition == 6) && (newY == 4)){
+            yPosition = newY;
+            return true;
+        }
+        else if((yPosition == 1) && (newY == 3)){
+            yPosition = newY;
+            return true;
+        }
+        else if((startingY == 6) && (newY != (yPosition - 1))){
+            return false;
+        }
+        else if((startingY == 1) && (newY != (yPosition + 1))){
+            return false;
+        }
+        yPosition = newY;
         return true;
     } 
     
     public boolean isBeatingAllowed(int newX, int newY){
+        if((newX == (xPosition + 1)) || (newX == (xPosition - 1) )){
+            if((startingY == 6) && (newY != yPosition - 1)){
+                return false;
+            }
+            if((startingY == 1) && (newY != yPosition + 1)){
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+        xPosition = newX;
+        yPosition = newY;
         return true;
     }
 }
