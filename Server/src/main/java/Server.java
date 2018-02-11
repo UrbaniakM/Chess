adc60064266a38a8ea3b3b353b3d52304dcb05e1
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2018-02-11 18:52:15 +0100

    -modify change variable privacy from private to package

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -2,28 +2,32 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Server {
-
-    private ServerSocket server = null;
+    private ServerSocket serverSocket = null;
     private ArrayList<Client> clients = null;
     private Socket clientSocket = null;
+    static Queue<Client> clientsWaiting = null;
 
-    private static final byte SEND_MOVE = 0;
-    private static final byte RECIVE_MOVE = 1;
-    private static final byte DECLARE_WINNING = 2;
-    private static final byte CONFIRM_OPPONNENT_AS_WINNER = 3;
-    private static final byte DECLINE_OPPONNENT_AS_WINNER = 4;
-    private static final byte SEARCH_FOR_NEW_GAME = 5;
-    private static final byte START_NEW_GAME = 6;
-    private static final byte ACCEPT_REMATCH = 7;
-    private static final byte DECLINE_REMATCH = 8;
+    static final byte SEND_MOVE = 0;
+    static final byte RECIVE_MOVE = 1;
+    static final byte DECLARE_WINNING = 2;
+    static final byte CONFIRM_OPPONNENT_AS_WINNER = 3;
+    static final byte DECLINE_OPPONNENT_AS_WINNER = 4;
+    static final byte SEARCH_FOR_NEW_GAME = 5;
+    static final byte START_NEW_GAME = 6;
+    static final byte ACCEPT_REMATCH = 7;
+    static final byte DECLINE_REMATCH = 8;
 
 
     public Server(){
         try{
-            server = new ServerSocket(3000);
+            serverSocket = new ServerSocket(3000);
             clients = new ArrayList<>();
+            clientsWaiting = new ArrayBlockingQueue<>(10);
+            //TODO: przy wychodzeniu clienta z servera, usuniecie tych czekajacych na nowa gre z listy
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(100);
@@ -33,7 +37,7 @@ public class Server {
     public void run(){
         while(true){
             try{
-                clientSocket = server.accept();
+                clientSocket = serverSocket.accept();
                 System.out.println("Connection Established, client IP: " + clientSocket.getInetAddress());
                 Client client = new Client(clientSocket);
                 client.start();
